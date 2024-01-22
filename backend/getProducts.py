deccb503db20b48de6119ff471ee0b52acbc9f4d
Rakshith <rakshith.ds10@gmail.com>
2024-01-21 22:43:15 -0600

    Removed Files

--- backend/getProducts.py
+++
@@ -1,9 +0,0 @@
-# import connection
-
-# client = connection.client
-
-# def products():
-#     mydb = client.project
-#     collection = mydb.products
-#     query = list(collection.find({},{"_id":0}))
-#     return query
